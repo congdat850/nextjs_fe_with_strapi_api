66b22331b2b9b65ac79b2117037637d8c5db6e61
Nguyen Cong Dat <nguyencongdat@192.168.2.70>
2022-08-05 16:18:36 +0700

    update _api

--- pages/api/_api/config/server.js
+++ pages/api/_api/config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bda0b0406c47552208207c4f08d5d555'),
+    },
   },
 });
---
+++ pages/api/_api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '184da678-0c55-47e5-a3ff-ae0d05c38f48'
+};
